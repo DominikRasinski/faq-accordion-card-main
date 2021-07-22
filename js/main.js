54ad28f11a47c3c0b749f13fa542ca60f0905a28
Dominik Rasiński <58070945+DominikRasinski@users.noreply.github.com>
2021-07-22 20:37:20 +0200

    remove debug code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 const button = document.querySelectorAll('.btn-arrow');
 const hiddenParagraph = document.querySelectorAll('.hiddenP');
 
-// console.log(button);
-
 for(const el of button){
     el.addEventListener("click", e =>{
         e.target.classList.toggle("btn-rotate");
         e.target.style.transition = '.2s';
         e.target.closest('.showP').classList.toggle('showP--fontWeight');
-        console.log(e.currentTarget);
         showHiddenParagraph(e);
     });
 }
@@ -16,8 +13,6 @@ function showHiddenParagraph(event){
     event.preventDefault();
 
     let paragraphVisible = event.currentTarget.closest('.showP');
-    console.log(paragraphVisible);
 
     let paragraphHidden = paragraphVisible.nextElementSibling.classList.toggle("showParagraph");
-    console.log(paragraphHidden);
 }
