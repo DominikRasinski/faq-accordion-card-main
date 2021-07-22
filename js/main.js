a89ef6b21e7eaa60936a47e822defe67be94a45d
Dominik Rasinski <dox.gmr@gmail.com>
2021-07-22 20:33:03 +0200

    improve my project

--- js/main.js
+++ js/main.js
@@ -7,5 +7,31 @@ for(const el of button){
     el.addEventListener("click", e =>{
         e.target.classList.toggle("btn-rotate");
         e.target.style.transition = '.2s';
+        e.target.closest('.showP').classList.toggle('showP--fontWeight');
+        console.log(e.currentTarget);
+        showHiddenParagraph(e);
     });
 }
+function showHiddenParagraph(event){
+    event.preventDefault();
+
+    let paragraphVisible = event.currentTarget.closest('.showP');
+    console.log(paragraphVisible);
+
+    let paragraphHidden = paragraphVisible.nextElementSibling.classList.toggle("showParagraph");
+    console.log(paragraphHidden);
+}
+
+
+// document.querySelectorAll('.myButton').forEach(function(button) {
+//     button.addEventListener('click', myFunction);
+//   });
+  
+//   function myFunction(evt) {
+//     evt.preventDefault();
+  
+//     var closest = evt.currentTarget.closest(".button").querySelector('.extra');
+  
+//     console.log(closest)
+  
+//   }
